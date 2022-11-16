b08273850bbe3667e14157a243884567aa0f2682
Killian <74175986+killianrochet@users.noreply.github.com>
2022-11-16 16:51:46 +0100

    Setup TF actions

---
+++ terraform/main.tf
@@ -0,0 +1,14 @@
+terraform {
+  cloud {
+    organization = "Ecom_Bluelion"
+
+    workspaces {
+      name = "Ecom_Bluelion"
+    }
+  }
+}
+
+provider “google” {
+  project = “ceri-m1-ecommerce-2022"
+  region  = “europe-west1”
+}
