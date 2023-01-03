8f3b9f3357be99d77ed902f205be733c3e951df7
Thomas LEGRAND <thomas.legrand@alumni.univ-avignon.fr>
2023-01-03 14:21:06 +0100

    Partial connection & re-organization of the files

---
+++ front/src/App.vue
@@ -0,0 +1,98 @@
+<template>
+    <GoogleLogin :callback="callbackResponse"/>
+
+    <nav>
+      <router-link to="/">Accueil</router-link> |
+      <router-link to="/cart">Panier</router-link>
+    </nav>
+    <router-view/>
+</template>
+
+<script>
+  // function decodeJwtResponse(token) {
+  //   let base64Url = token.split('.')[1]
+  //   let base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+  //   let jsonPayload = decodeURIComponent(atob(base64).split('').map(function(c) {
+  //       return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+  //   }).join(''));
+  //   return JSON.parse(jsonPayload)
+  // }
+
+  const callbackResponse = (response) => {
+    // This callback will be triggered when the user selects or login to
+    // his Google account from the popup
+    console.log("Handle the response", response)
+  }
+
+  // let responsePayload;
+  // console.log(responsePayload);
+  // window.handleCalback = (response) => {
+  //   responsePayload = callbackResponse(response.credential);
+  // }
+
+  // let responsePayload;
+  // console.log(responsePayload);
+  // window.handleCredentialResponse = (response) => {
+  //   // decodeJwtResponse() is a custom function defined by you
+  //   // to decode the credential response.
+  //   responsePayload = decodeJwtResponse(response.credential);
+
+  //   console.log("ID: " + responsePayload.sub);
+  //   console.log('Full Name: ' + responsePayload.name);
+  //   console.log('Given Name: ' + responsePayload.given_name);
+  //   console.log('Family Name: ' + responsePayload.family_name);
+  //   console.log("Image URL: " + responsePayload.picture);
+  //   console.log("Email: " + responsePayload.email);
+  // }
+
+  export default {
+    mounted() {
+      this.$store.commit('updateCartFromLocalStorage'); // Changed commit to dispatch
+    }
+  };
+</script>
+
+<style lang="scss">
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: white;
+  }
+
+  html, body {
+    margin: 0;
+    padding: 0;
+    overflow-x: hidden;
+  }
+
+  body {
+    background-color: #282828;
+  }
+
+  nav {
+    padding: 10px;
+    width: 100%;
+    height: 30px;
+    background-color: #404040;
+    line-height: 30px;
+
+    a {
+      font-weight: bold;
+      color: white;
+      text-decoration: none;
+      margin: 0 5px 0 5px;
+      font-size: 1.25rem;
+
+      &.router-link-exact-active {
+        color: orange;
+        text-decoration: underline;
+      }
+    }
+  }
+
+  .text-center {
+    text-align: center;
+  }
+</style>
