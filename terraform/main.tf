283680bcef91776eedd789c32b244bc8221a5770
Killian <74175986+killianrochet@users.noreply.github.com>
2023-01-03 14:29:06 +0100

    Modification du fichier terrafort

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,8 +8,44 @@ terraform {
   }
 }
 
+data "google_secret_manager_secret" "host" {
+  secret_id = "mysql-address"
+}
+
+data "google_secret_manager_secret" "user" {
+  secret_id = "mysql-user-bluelion"
+}
+
+data "google_secret_manager_secret" "password" {
+  secret_id = "mysql-password-bluelion"
+}
+
+data "google_secret_manager_secret" "dbname" {
+  secret_id = "mysql-database-bluelion"
+}
+
 provider "google" {
   project = "ceri-m1-ecommerce-2022"
   region  = "europe-west1"
+}
+
+
+resource "google_cloud_run_service" "bluelion-backend" {
+  provider = google
+  name         = "bluelion-backend"
+  location     = "europe-west1"
+  template {
+    spec {
+      service_account_name = "terraform-bluelion@ceri-m1-ecommerce-2022.iam.gserviceaccount.com"
+      containers {
+        image = "europe-west1-docker.pkg.dev/ceri-m1-ecommerce-2022/bluelion"
+      }
+    }
+    metadata {
+      annotations = {
+        "autoscaling.knative.dev/maxScale" = "1"
+      }
+    }
+  }
 }
 
