b0e7ed63207a9c2d3696fd446b404b73d595380e
Raphael Rey <raphael.rey@alumni.univ-avignon.fr>
2023-01-09 16:51:14 +0100

    zhonya

--- app/main.py
+++ app/main.py
@@ -17,13 +17,25 @@ DB_USER = os.environ["DB_USER"]
 DB_PASS = os.environ["DB_PASS"]
 DB_NAME = os.environ["DB_NAME"]
 INSTANCE_CONNECTION_NAME = os.environ["INSTANCE_CONNECTION_NAME"]
-conn = connector.connect(
-        INSTANCE_CONNECTION_NAME,
-        "pymysql",
-        user=DB_USER,
-        password=DB_PASS,
-        db=DB_NAME
-    )
+#conn = connector.connect(
+#        INSTANCE_CONNECTION_NAME,
+#        "pymysql",
+#        user=DB_USER,
+#        password=DB_PASS,
+#        db=DB_NAME
+#    )
+
+
+DATABASE_URL = sqlalchemy.engine.url.URL.create(
+    drivername="mysql+pymysql",
+    username=DB_USER,
+    password=DB_PASS,
+    database=DB_NAME,
+    query={"unix_socket": "/cloudsql/ceri-m1-ecommerce-2022:europe-west1:mysql-primary"},
+)
+# sqlite_file_name = "database.db"
+# sqlite_url = f"sqlite:///{sqlite_file_name}"
+engine = create_engine(DATABASE_URL)
 app = FastAPI()
 
 #CREATE TABLE artists(ID int NOT NULL,name varchar(255),PRIMARY KEY(ID));
@@ -34,7 +46,9 @@ app = FastAPI()
 
 
 #engine = create_engine("mysql://root:supersecret@127.0.0.1:3306/music")
-engine = create_engine("mysql+pymysql://",creator=conn)
+#engine = create_engine("mysql+pymysql://",creator=conn)
+
+
 class Artists(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
     name: str
