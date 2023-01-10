78cc15984c6df15f4d7bd4ce7e641f91b6cb1460
Killian Rochet <rochetkillianpro@icloud.com>
2023-01-10 19:53:20 +0100

    stp marche

--- terraform/main.tf
+++ terraform/main.tf
@@ -47,7 +47,7 @@ resource "google_cloud_run_service" "bluelion-backend" {
     spec {
       service_account_name = "terraform-bluelion@ceri-m1-ecommerce-2022.iam.gserviceaccount.com"
       containers {
-        image = "europe-west1-docker.pkg.dev/ceri-m1-ecommerce-2022/bluelion/backend:0.0.16"
+        image = "europe-west1-docker.pkg.dev/ceri-m1-ecommerce-2022/bluelion/backend:0.0.17"
         env {
           name = "INSTANCE_CONNECTION_NAME"
           value = "ceri-m1-ecommerce-2022:europe-west1:mysql-primary"
@@ -104,11 +104,14 @@ resource "google_cloud_run_service" "bluelion-frontend" {
     spec {
       service_account_name = "terraform-bluelion@ceri-m1-ecommerce-2022.iam.gserviceaccount.com"
       containers {
-        image = "europe-west1-docker.pkg.dev/ceri-m1-ecommerce-2022/bluelion/frontend:0.0.16"
+        image = "europe-west1-docker.pkg.dev/ceri-m1-ecommerce-2022/bluelion/frontend:0.0.17"
         env {
           name = "BACKEND_URL"
           value = google_cloud_run_service.bluelion-backend.status[0].url
         }
+        ports {
+          container_port = 5000
+        } 
       }
     }
 
