5665116b400cb96d69e907b3688cc5f44ba1b512
Killian Rochet <rochetkillianpro@icloud.com>
2023-01-10 16:44:08 +0100

    port modif

--- terraform/main.tf
+++ terraform/main.tf
@@ -110,7 +110,7 @@ resource "google_cloud_run_service" "bluelion-frontend" {
           value = google_cloud_run_service.bluelion-backend.status[0].url
         }
         ports {
-          container_port = 80
+          container_port = 5000
         }
       }
     }
